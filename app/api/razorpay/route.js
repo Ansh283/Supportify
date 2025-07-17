49778d286d9f311517fb546755c18614a2c6a45b
Ansh Pal <anshvpal@gmail.com>
2025-07-17 19:31:28 +0530

    Add fallback for Razorpay secret in verification

--- app/api/razorpay/route.js
+++ app/api/razorpay/route.js
@@ -18,7 +18,8 @@ export const POST = async (req) => {
 
     //fetch the secret of the user who is gettkig the payment
     let user = await User.findOne({username: p.to_user})
-    const secret = user.razorpaysecret
+    const secret = user.razorpaysecret || process.env.RAZORPAY_KEY_SECRET
+
 
     //Verify the payment
     let xx = validatePaymentVerification({"order_id": body.razorpay_order_id, "payment_id": body.razorpay_payment_id}, body.razorpay_signature, secret)
